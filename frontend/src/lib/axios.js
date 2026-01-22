cc2166d002a96288c5e4816b628b8dcc787980e3
Nandana K <nandanakodiveettil@gmail.com>
2026-01-22 13:01:53 +0530

    fix: use backend url in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-1-g1rv.onrender.com/" : "/api",
   withCredentials: true,
 });
