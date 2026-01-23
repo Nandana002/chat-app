cf741d0db1f43a99322bf3b4eadb3bca4146f0fa
Nandana K <nandanakodiveettil@gmail.com>
2026-01-23 10:55:52 +0530

    small changes in axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL ?? (import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chat-app-1-g1rv.onrender.com/api");
-
 export const axiosInstance = axios.create({
-  baseURL,
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-1-g1rv.onrender.com/api" : "/api",
   withCredentials: true,
 });
